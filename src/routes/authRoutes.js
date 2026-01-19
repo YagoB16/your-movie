c13c7a476fb1b78aec1e58e8c76b4ad1a4fe89be
yago <ybarbosa1608@gmail.com>
2026-01-19 14:22:02 -0300

    feat: adiciona validação de registro de usuário com Zod na rota de registro

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,9 +1,27 @@
-import express from 'express';
-import { login, register } from '../controllers/authController.js';
-
+import express from "express";
+import { login, register } from "../controllers/authController.js";
+import { validate } from "../middlewares/validateMiddleware.js";
+import {registerSchema} from "../services/registerValidation.js"
 const router = express.Router();
 
-router.post('/register', register);
-router.post('/login', login);
+/**
+ * @api {post} /auth/register Registrar Usuário
+ * @apiName RegisterUser
+ * @apiGroup Autenticação
+ *
+ * @apiBody {String} email E-mail válido do usuário.
+ * @apiBody {String} senha Senha (mínimo 6 caracteres).
+ * @apiBody {String="admin","user"} [role="user"] Nível de acesso.
+ *
+ * @apiSuccess (Sucesso 201) {Boolean} success Status da operação.
+ * @apiSuccess (Sucesso 201) {String} message Mensagem de sucesso.
+ * @apiSuccess (Sucesso 201) {String} token JWT gerado para o usuário.
+ *
+ * @apiError (Erro 400) {String} status Erro de Validação.
+ * @apiError (Erro 400) {Object[]} erros Detalhes dos campos inválidos.
+ */
+router.post("/register", validate(registerSchema), register);
+
+router.post("/login", login);
 
 export default router;
