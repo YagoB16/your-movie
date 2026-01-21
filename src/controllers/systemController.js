afd6578d44deac529dd078a5e631ccf1c95ce479
yago <ybarbosa1608@gmail.com>
2026-01-21 19:28:31 -0300

    feat: implementa verificação de saúde do servidor com conexão ao banco de dados

--- src/controllers/systemController.js
+++ src/controllers/systemController.js
@@ -1,7 +1,27 @@
-export const checkServer = (req, res) => {
-    res.status(200).json({
-        status: "Online",
-        timestamp: new Date(),
-        environment: process.env.NODE_ENV || 'development'
-    });
+import { db } from "../config/database.js";
+import { collection, getDocs, limit, query } from "firebase/firestore";
+
+export const checkServer = async (req, res) => {
+  const healthcheck = {
+    uptime: process.uptime(),
+    message: 'OK',
+    timestamp: Date.now(),
+    services: {
+      database: 'PENDING'
+    }
+  };
+
+  try {
+ 
+    const q = query(collection(db, "movies"), limit(1));
+    await getDocs(q);
+
+    healthcheck.services.database = 'CONNECTED';
+    res.status(200).json(healthcheck);
+  } catch (error) {
+    healthcheck.message = 'ERROR';
+    healthcheck.services.database = 'DISCONNECTED';
+    healthcheck.error = error.message;
+    res.status(503).json(healthcheck); // 503 Service Unavailable
+  }
 };
