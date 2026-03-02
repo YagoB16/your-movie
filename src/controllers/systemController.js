dd0e26290102ba0605b5b8eddd4df5eea423b30a
Yago Barbosa <91913602+YagoB16@users.noreply.github.com>
2026-03-02 12:07:48 -0300

    fix: add return in health check

--- src/controllers/systemController.js
+++ src/controllers/systemController.js
@@ -12,16 +12,16 @@ export const checkServer = async (req, res) => {
   };
 
   try {
- 
+
     const q = query(collection(db, "movies"), limit(1));
     await getDocs(q);
 
     healthcheck.services.database = 'CONNECTED';
-    res.status(200).json(healthcheck);
+    return res.status(200).json(healthcheck);
   } catch (error) {
     healthcheck.message = 'ERROR';
     healthcheck.services.database = 'DISCONNECTED';
     healthcheck.error = error.message;
-    res.status(503).json(healthcheck); // 503 Service Unavailable
+    return res.status(503).json(healthcheck); // 503 Service Unavailable
   }
 };
