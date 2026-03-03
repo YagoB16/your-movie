5355cfc12768fa89a54a6086d33a991910d7b30c
Yago Barbosa <91913602+YagoB16@users.noreply.github.com>
2026-03-03 14:17:14 -0300

    Fix privateKey formatting in database.js

--- src/config/database.js
+++ src/config/database.js
@@ -5,7 +5,7 @@ const serviceAccount = {
   projectId: process.env.FIREBASE_PROJECT_ID,
   clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
   // Remove aspas acidentais e converte \n em quebras de linha reais
-  privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/"/g, '').replace(/\\n/g, '\n'),
+  privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n').replace(/"/g, '')
 };
 
 if (!admin.apps.length) {
